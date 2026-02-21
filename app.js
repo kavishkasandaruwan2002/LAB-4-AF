d555d43bda7c65a7c2bbdc32b05d37635929f366
kavishkasandaruwan2002 <kavishkasandaruwan608@gmail.com>
2026-02-21 22:59:05 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello from Node + GitHub flow");
+const myPromise = Promise.resolve("Success!");
+
+async function myFunction() {
+  try {
+    const result = await myPromise;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+myFunction();
